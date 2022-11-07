70e7d46810853c25b775d6d9d81295784cb76893
AnoshkinRI <anoshkinr@mail.ru>
2022-11-07 13:17:36 +0300

    add new parameterized realization

--- pet_store_api/src/test/java/tests/GetPetByIdTests.java
+++ pet_store_api/src/test/java/tests/GetPetByIdTests.java
@@ -5,17 +5,19 @@ import dto.PetModel;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import tests.dataproviders.JsonToList;
 import utils.ResponseWrapper;
 
 import java.util.stream.Collectors;
 
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
-import static utils.BodyGenerator.petObject;
 
 @Epic("PetStore")
 @Story("Pet")
@@ -25,17 +27,17 @@ public class GetPetByIdTests extends Initialization {
 
     private PetModel createPetBody;
 
-    @BeforeEach
-    void create_pet() {
-        createPetBody = petObject();
-
-        PetModel response = petApi.createPet(createPetBody)
-                .printResponseToConsole()
-                .assertStatusCode(HttpStatus.SC_OK)
-                .as(PetModel.class);
-
-        petId = response.getId();
-    }
+//    @BeforeEach
+//    void create_pet() {
+//        createPetBody = petObject();
+//
+//        PetModel response = petApi.createPet(createPetBody)
+//                .printResponseToConsole()
+//                .assertStatusCode(HttpStatus.SC_OK)
+//                .as(PetModel.class);
+//
+//        petId = response.getId();
+//    }
 
     @Test
     @DisplayName("Успешное получение животного из магазина")
@@ -72,7 +74,7 @@ public class GetPetByIdTests extends Initialization {
     @ParameterizedTest(name = "{displayName} - {index}")
     @DisplayName("Получение животного из json-файла")
     @MethodSource("tests.dataproviders.DataProviders#get_pet_from_json")
-    void get_pet_from_json(PetModel model) {
+    void get_pet_from_json_use_provider(PetModel model) {
         PetModel response = petApi.getPet(model.getId())
                 .printResponseToConsole()
                 .assertStatusCode(HttpStatus.SC_OK)
@@ -97,5 +99,38 @@ public class GetPetByIdTests extends Initialization {
                         assertThat(x).as("id").hasFieldOrPropertyWithValue("id", createPetBody.getTags().stream().map(PetModel.TagsItem::getId).collect(Collectors.toList()));
                     });
                 });
+    }
+
+    @ParameterizedTest(name = "{displayName} - {index}")
+    @DisplayName("Получение животного из json-файла")
+    @CsvSource({"src/test/resources/data.json, dto.PetModel"})
+    void get_pet_from_json_use_aggregator(@AggregateWith(JsonToList.class) PetModel[] models) {
+
+        for (PetModel model : models) {
+            PetModel response = petApi.getPet(model.getId())
+                    .printResponseToConsole()
+                    .assertStatusCode(HttpStatus.SC_OK)
+                    .as(PetModel.class);
+
+            step("Проверка ответа у питомца с id = " + model.getId(), () -> ResponseWrapper
+                    .assertSoftly(softly -> {
+                        softly.assertThat(response.getId()).withFailMessage("petId is null!")
+                                .isNotNull();
+                        softly.assertThat(response.getName()).withFailMessage("Pet name <%s> is not equal to <%s>", response.getName(), model.getName())
+                                .isEqualTo(model.getName());
+                        softly.assertThat(response.getStatus()).withFailMessage("Status <%s> is not equal to <%s>", response.getStatus(), model.getStatus())
+                                .isEqualTo(model.getStatus());
+                        softly.assertThat(response.getCategory().getName())
+                                .withFailMessage("Category name <%s> is not equal to <%s>", response.getCategory().getName(), model.getCategory().getName())
+                                .isEqualTo(model.getCategory().getName());
+                        softly.assertThat(response.getCategory().getId())
+                                .withFailMessage("Category id <%s> is not equal to <%s>", response.getCategory().getId(), model.getCategory().getId())
+                                .isEqualTo(model.getCategory().getId());
+                        softly.assertThat(response.getTags()).withFailMessage("Not find expected tags!").extracting("name", "id").asList().filteredOnAssertions(x -> {
+                            assertThat(x).as("name").hasFieldOrPropertyWithValue("name", model.getTags().stream().map(PetModel.TagsItem::getName).collect(Collectors.toList()));
+                            assertThat(x).as("id").hasFieldOrPropertyWithValue("id", model.getTags().stream().map(PetModel.TagsItem::getId).collect(Collectors.toList()));
+                        });
+                    }));
+        }
     }
 }
---
+++ pet_store_api/src/test/java/tests/dataproviders/JsonToList.java
@@ -0,0 +1,25 @@
+package tests.dataproviders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JsonToList implements ArgumentsAggregator {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+        try {
+            return objectMapper.readValue(new File(accessor.getString(0)), Class.forName(accessor.getString(1)).arrayType());
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        throw new NullPointerException("List is null!");
+    }
+}
