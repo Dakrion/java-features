25d87df7b8edc1c53da8a7b4208c20a5f5b829b4
AnoshkinRI <anoshkinr@mail.ru>
2022-12-06 16:53:42 +0300

    fix validation

--- pet_store_api/src/test/java/restclient/PetClient.java
+++ pet_store_api/src/test/java/restclient/PetClient.java
@@ -4,11 +4,13 @@ import exceptions.AnnotationException;
 import io.qameta.allure.Step;
 import io.restassured.specification.RequestSpecification;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import utils.ResponseWrapper;
 import utils.annotations.restspec.DELETE;
 import utils.annotations.restspec.GET;
 import utils.annotations.restspec.POST;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 import static io.restassured.RestAssured.given;
@@ -24,51 +26,52 @@ public class PetClient implements PetApi {
     private final Class<PetApi> api = PetApi.class;
 
     @Step("Get pet by Id")
+    @SneakyThrows
     public ResponseWrapper getPet(Long petId) {
-        try {
-            Method thisMethod = api.getDeclaredMethod("getPet", Long.class);
-            if (thisMethod.isAnnotationPresent(GET.class)) {
-                return new ResponseWrapper(given(requestSpecification)
-                        .when()
-                        .get(thisMethod.getAnnotation(GET.class).endpoint(), petId)
-                        .thenReturn());
-            } else throw new AnnotationException("No annotation for this method!");
-        } catch (NoSuchMethodException ex) {
-            ex.printStackTrace();
-        }
-        throw new RuntimeException("Failed to execute this method!");
+            return new ResponseWrapper(given(requestSpecification)
+                    .when()
+                    .get(validateAnnotation("getPet", api, GET.class, Long.class).endpoint(), petId)
+                    .thenReturn());
     }
 
     @Step("Add pet to store")
+    @SneakyThrows
     public <T> ResponseWrapper createPet(T body) {
-        try {
-            Method thisMethod = api.getDeclaredMethod("createPet", Object.class);
-            if (thisMethod.isAnnotationPresent(POST.class)) {
-                return new ResponseWrapper(given(requestSpecification)
-                        .body(body)
-                        .when()
-                        .post(thisMethod.getAnnotation(POST.class).endpoint())
-                        .thenReturn());
-            } else throw new AnnotationException("No annotation for this method!");
-        } catch (NoSuchMethodException ex) {
-            ex.printStackTrace();
-        }
-        throw new RuntimeException("Failed to execute this method!");
+            return new ResponseWrapper(given(requestSpecification)
+                    .body(body)
+                    .when()
+                    .post(validateAnnotation("createPet", api, POST.class, Object.class).endpoint())
+                    .thenReturn());
     }
 
     @Step("Delete pet from store")
+    @SneakyThrows
     public ResponseWrapper deletePet(Long petId) {
+            return new ResponseWrapper(given(requestSpecification)
+                    .when()
+                    .delete(validateAnnotation("deletePet", api, DELETE.class, Long.class).endpoint(), petId)
+                    .thenReturn());
+    }
+
+    /**
+     * Method, which check the annotation for method in interface
+     *
+     * @param methodName      - name of method
+     * @param annotationClass - annotation for this method
+     * @param parameterTypes  - parameter types for method
+     * @return {@link Annotation}
+     */
+    public static <T extends Annotation> T validateAnnotation(final String methodName, final Class<?> feature,
+                                                              final Class<T> annotationClass,
+                                                              final Class<?>... parameterTypes) {
         try {
-            Method thisMethod = api.getDeclaredMethod("deletePet", Long.class);
-            if (thisMethod.isAnnotationPresent(DELETE.class)) {
-                return new ResponseWrapper(given(requestSpecification)
-                        .when()
-                        .delete(thisMethod.getAnnotation(DELETE.class).endpoint(), petId)
-                        .thenReturn());
-            } else throw new AnnotationException("No annotation for this method!");
+            Method thisMethod = feature.getDeclaredMethod(methodName, parameterTypes);
+            if (thisMethod.isAnnotationPresent(annotationClass)) {
+                return thisMethod.getAnnotation(annotationClass);
+            }
         } catch (NoSuchMethodException ex) {
             ex.printStackTrace();
         }
-        throw new RuntimeException("Failed to execute this method!");
+        throw new AnnotationException("No annotation for this method!");
     }
 }
